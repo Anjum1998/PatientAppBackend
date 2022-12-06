d70731335fc58611543e4274eac90383df05f7c5
Anju M <Anju@NTP-LAP-771.chn.nestgroup.net>
2022-12-06 21:47:05 +0530

    model

--- src/main/java/com/example/PatientApp_Backend/controller/PatientController.java
+++ src/main/java/com/example/PatientApp_Backend/controller/PatientController.java
@@ -1,17 +1,20 @@
 package com.example.PatientApp_Backend.controller;
 
+import com.example.PatientApp_Backend.dao.PatientDao;
 import com.example.PatientApp_Backend.model.Patient;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class PatientController {
+    @Autowired
+    private PatientDao dao;
 
-    @PostMapping(path ="/add",consumes = "application/json",produces ="application/json")
-    public String AddPatient(@RequestBody Patient p)
-    {
+    @CrossOrigin(origins = "*")
+    @PostMapping(path = "/add", consumes = "application/json", produces = "application/json")
+    public String AddPatient(@RequestBody Patient p) {
         System.out.println(p.getName().toString());
         System.out.println(p.getPid());
         System.out.println(p.getAddress().toString());
@@ -19,11 +22,12 @@ public class PatientController {
         System.out.println(p.getDoa().toString());
         System.out.println(p.getImage().toString());
         System.out.println(p.getDname().toString());
+        dao.save(p);
         return "patient added successfully";
     }
+    @CrossOrigin(origins = "*")
     @GetMapping("/view")
-    public String ViewPatient()
-    {
-        return "Welcome to view all";
+    public List<Patient> ViewPatient() {
+        return (List<Patient>) dao.findAll();
     }
 }
