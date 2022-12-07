97eacf203ff0ee26b86f159e48df2c04314ee124
Anju M <Anju@NTP-LAP-771.chn.nestgroup.net>
2022-12-07 10:36:39 +0530

    hashmap

--- src/main/java/com/example/PatientApp_Backend/controller/PatientController.java
+++ src/main/java/com/example/PatientApp_Backend/controller/PatientController.java
@@ -5,7 +5,9 @@ import com.example.PatientApp_Backend.model.Patient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 public class PatientController {
@@ -14,7 +16,7 @@ public class PatientController {
 
     @CrossOrigin(origins = "*")
     @PostMapping(path = "/add", consumes = "application/json", produces = "application/json")
-    public String AddPatient(@RequestBody Patient p) {
+    public Map<String,String> AddPatient(@RequestBody Patient p) {
         System.out.println(p.getName().toString());
         System.out.println(p.getPid());
         System.out.println(p.getAddress().toString());
@@ -23,8 +25,11 @@ public class PatientController {
         System.out.println(p.getImage().toString());
         System.out.println(p.getDname().toString());
         dao.save(p);
+        HashMap<String, String> map=new HashMap<>();
+        map.put("status","success");
+        return map;
 
-        return "patient added successfully";
+
     }
     @CrossOrigin(origins = "*")
     @GetMapping("/view")
